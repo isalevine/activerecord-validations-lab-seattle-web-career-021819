4a3bd4936a934dcbc5306ea9c6480f6461516ae9
isalevine <isaacdlevine@gmail.com>
2019-03-20 11:49:56 -0700

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  has_many :posts
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, numericality: {only_integer: true}, length: {is: 10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,38 @@
 class Post < ActiveRecord::Base
+  belongs_to :author
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :clickbait_title
+
+  def clickbait_title
+    if title
+      check = ["Won't Believe", "Secret", "Top", "Guess"].any? do |clickbait|
+        title.include?(clickbait)
+      end
+
+      if check == false
+        errors.add(:title, "title is not clickbaity enough!")
+      end
+    end
+  end
+
+
+
+  # def clickbait_title
+  #   # binding.pry
+  #   output = false
+  #   if title
+  #     ["Won't Believe", "Secret", "Top", "Guess"].each do |clickbait|
+  #       if title.include?(clickbait)
+  #         output = true
+  #       end
+  #     end
+  #   end
+  #   if output == false
+  #     errors.add(:title, "title is not clickbaity enough!")
+  #   end
+  # end
+
 end
